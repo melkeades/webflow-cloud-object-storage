bb3e94ca482074ec7965d25d3ce34bf83cb9a71b
melkeades <118681335+melkeades@users.noreply.github.com>
2025-11-24 01:14:50 +0300

    l

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -7,7 +7,7 @@ export const API = {
   corsHeaders: {
     "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
     "Access-Control-Allow-Headers": "Content-Type, Authorization",
-    "Access-Control-Allow-Origin": "*",
+    // "Access-Control-Allow-Origin": "*",
   },
 
   // Merge provided headers with CORS defaults and dynamic origin handling
